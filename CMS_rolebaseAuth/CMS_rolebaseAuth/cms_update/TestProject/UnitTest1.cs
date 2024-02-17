a4c7f231dbbf49c11453f4b2a8f1652d0a8228b3
neojarvis <techadmin@iamneo.ai>
2024-02-17 13:43:31 +0000

    Auto-save-commit-17/02/2024--19:13:33

--- CMS_rolebaseAuth/CMS_rolebaseAuth/cms_update/TestProject/UnitTest1.cs
+++ CMS_rolebaseAuth/CMS_rolebaseAuth/cms_update/TestProject/UnitTest1.cs
@@ -2,27 +2,22 @@ using NUnit.Framework;
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using dotnetapp.Models;
 
 [TestFixture]
 public class SpringappApplicationTests
 {
     private HttpClient _httpClient;
-    private string _generatedToken;
 
     [SetUp]
     public void Setup()
     {
         _httpClient = new HttpClient();
-        _httpClient.BaseAddress = new Uri("https://8080-aabdbffdadabafcfdbcfacbdcbaeadbebabcdebdca.premiumproject.examly.io/"); 
+        _httpClient.BaseAddress = new Uri("https://8080-aabdbffdadabafcfdbcfacbdcbaeadbebabcdebdca.premiumproject.examly.io");
     }
-
-
-
-
 
     [Test, Order(1)]
     public async Task Backend_TestRegisterUser()
@@ -33,14 +28,35 @@ public class SpringappApplicationTests
         string uniqueUsername = $"abcd_{uniqueId}";
         string uniqueEmail = $"abcd{uniqueId}@gmail.com";
 
-
-        string requestBody = $"{{\"password\": \"abc@123A\",\"email\": \"{uniqueEmail}\"}}";
-        HttpResponseMessage response = await _httpClient.PostAsync("/user/login", new StringContent(requestBody, Encoding.UTF8, "application/json"));
-        Console.WriteLine(response.Content);
-        string responseString = await response.Content.ReadAsStringAsync();
+        string requestBody = $"{{\"Username\": \"{uniqueUsername}\", \"Password\": \"abc@123A\", \"Email\": \"{uniqueEmail}\", \"MobileNumber\": \"1234567890\", \"UserRole\": \"Operator\"}}";
+        HttpResponseMessage response = await _httpClient.PostAsync("/api/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
 
         Console.WriteLine(response.StatusCode);
+        string responseString = await response.Content.ReadAsStringAsync();
+
         Console.WriteLine(responseString);
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
     }
+
+    [Test, Order(2)]
+    public async Task Backend_TestLoginAdmin()
+    {
+        string uniqueId = Guid.NewGuid().ToString();
+
+        string uniqueusername = $"abcd_{uniqueId}";
+        string uniquepassword = $"abcdA{uniqueId}@123";
+        string uniquerole = "Admin"; // Adjusted to match the role check in the controller
+        string requestBody = $"{{\"Username\" : \"{uniqueusername}\",\"Password\" : \"{uniquepassword}\",\"UserRole\" : \"{uniquerole}\" }}";
+
+        HttpResponseMessage response = await _httpClient.PostAsync("/api/login", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        string requestBody1 = $"{{\"Email\" : \"{uniqueusername}\",\"Password\" : \"{uniquepassword}\"}}";
+        HttpResponseMessage response1 = await _httpClient.PostAsync("/api/login", new StringContent(requestBody1, Encoding.UTF8, "application/json"));
+        Assert.AreEqual(HttpStatusCode.OK, response1.StatusCode);
+
+        string responseBody = await response1.Content.ReadAsStringAsync();
+        // Add assertions based on the response content if needed
+    }
+
 }
