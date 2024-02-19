a1077e13fe4f2653ea4d0ca36207bf973123d018
neojarvis <techadmin@iamneo.ai>
2024-02-19 10:35:24 +0000

    Auto-save-commit-19/02/2024--16:05:19

---
+++ British/angularapp/src/app/models/course.model.ts
@@ -0,0 +1,8 @@
+export interface Course {
+    courseID: number;
+    courseName: string;
+    description: string;
+    duration: string;
+    amount: number;
+  }
+  
---
+++ British/angularapp/src/app/models/enquiry.model.ts
@@ -0,0 +1,11 @@
+export interface Enquiry {
+    enquiryID: number;
+    enquiryDate: Date;
+    userId: string;
+    title: string;
+    description: string;
+    emailID: string;
+    enquiryType: string;
+    courseName: string;
+  }
+  
