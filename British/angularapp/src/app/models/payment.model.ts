42ba6703460e3aaf778cbddfbda9e7f886c79cb8
neojarvis <techadmin@iamneo.ai>
2024-02-20 10:22:03 +0000

    Auto-save-commit-20/02/2024--15:52:01

---
+++ British/angularapp/src/app/models/payment.model.ts
@@ -0,0 +1,14 @@
+import { Course } from "./course.model";
+import { User } from "./user.model";
+
+export interface Payment {
+    paymentID: number;
+    userId?: number;
+    courseID?: number;
+    amountPaid: number;
+    paymentDate: Date;
+    paymentMethod: string;
+    transactionID: string;
+    user?: User;
+    course?: Course;
+  }
--- British/dotnetapp/models/Payment.cs
+++ British/dotnetapp/models/Payment.cs
@@ -8,8 +8,8 @@ public class Payment
 {
     [Key]
     public int PaymentID { get; set; }
-    public long UserId { get; set; }
-    public int CourseID { get; set; }
+    public long ?UserId { get; set; }
+    public int ?CourseID { get; set; }
     public decimal AmountPaid { get; set; }
     public DateTime PaymentDate { get; set; }
     public string PaymentMethod { get; set; }
@@ -17,10 +17,10 @@ public class Payment
     
     // Navigation property for the related user
     [ForeignKey(nameof(UserId))]
-    public User User { get; set; }
+    public User? User { get; set; }
 
     // Navigation property for the related course
     [ForeignKey(nameof(CourseID))]
-    public Course Course { get; set; }
+    public Course ?Course { get; set; }
 }
 }
