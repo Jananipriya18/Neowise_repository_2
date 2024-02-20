b53cd243ee0455ee2da52ee116f15f6624a86f4d
neojarvis <techadmin@iamneo.ai>
2024-02-20 09:56:20 +0000

    Auto-save-commit-20/02/2024--15:26:19

--- British/angularapp/src/app/components/enquiry-form/enquiry-form.component.ts
+++ British/angularapp/src/app/components/enquiry-form/enquiry-form.component.ts
@@ -54,6 +54,7 @@ export class EnquiryFormComponent implements OnInit {
   
 
 createEnquiry(): void {
+  // console.log(course)
   console.log(this.authService.isCustomer(),this.newEnquiryForm.valid)
   if (this.authService.isCustomer() && this.newEnquiryForm.valid) {
     const newEnquiry: Enquiry = this.newEnquiryForm.value as Enquiry;
