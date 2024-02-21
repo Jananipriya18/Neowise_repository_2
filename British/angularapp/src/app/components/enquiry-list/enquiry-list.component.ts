f5c9dac7fbd839225ed9e87c76d7ad59c5bb8207
neojarvis <techadmin@iamneo.ai>
2024-02-21 10:28:05 +0000

    Auto-save-commit-21/02/2024--15:58:05

--- British/angularapp/src/app/components/enquiry-list/enquiry-list.component.ts
+++ British/angularapp/src/app/components/enquiry-list/enquiry-list.component.ts
@@ -1,4 +1,5 @@
 // enquiry-list.component.ts
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Enquiry } from 'src/app/models/enquiry.model';
 import { EnquiryService } from 'src/app/services/enquiry.service';
@@ -21,11 +22,22 @@ export class EnquiryListComponent implements OnInit {
     this.enquiryService.getAllEnquiries().subscribe(
       (enquiries: Enquiry[]) => {
         this.enquiries = enquiries;
-        console.log(this.enquiries);
+        console.log('Enquiries:', this.enquiries);
       },
       (error) => {
         console.error('Error fetching enquiries:', error);
+  
+        // Log additional details if available
+        if (error instanceof HttpErrorResponse) {
+          console.error('Status:', error.status);
+          console.error('Status Text:', error.statusText);
+          console.error('Response:', error.error);
+        }
+
+        // Log additional details about the error
+        console.error('Name:', error.name);
+        console.error('Message:', error.message);
       }
     );
-  }
+  }  
 }
