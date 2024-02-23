9cacf96a5a1d8f793e6a67a58da7a44caff4e878
neojarvis <techadmin@iamneo.ai>
2024-02-23 14:52:56 +0000

    Auto-save-commit-23/02/2024--20:22:54

--- British/angularapp/src/app/components/course-list/course-list.component.ts
+++ British/angularapp/src/app/components/course-list/course-list.component.ts
@@ -17,7 +17,7 @@ export class CourseListComponent implements OnInit {
   courses: Course[] = [];
   selectedCourse: Course;
   deleteConfirmationState: { [key: number]: boolean } = {};
-  isCustomer: boolean = false;
+  isAdmin: boolean = false;
   
 
   constructor(
@@ -31,7 +31,7 @@ export class CourseListComponent implements OnInit {
     this.getAllCourses();
 
     this.authService.userRole$.subscribe(role => {
-      this.isCustomer = role === 'Customer';
+      this.isAdmin = role === 'Admin';
 
     });
     // Initialize the form controls
@@ -60,7 +60,98 @@ export class CourseListComponent implements OnInit {
     );
   }
 
-     
+  updateCourse(courseID: number): void {
+    if (this.authService.isAdmin()) {
+      this.selectedCourse = this.courses.find(course => course.courseID === courseID);
+  
+      if (this.selectedCourse) {
+        console.log('Selected Course:', this.selectedCourse);
+        // Reset the form to clear any previous values
+        this.editCourseForm.reset();
+        // Patch the form values with the selected course
+        this.editCourseForm.patchValue({
+          courseName: this.selectedCourse.courseName,
+          description: this.selectedCourse.description,
+          duration: this.selectedCourse.duration,
+          amount: this.selectedCourse.amount,
+        });
+        this.editCourseModalVisible = true;
+      } else {
+        console.error('Course not found');
+      }
+    } else {
+      console.error('Only admins can update courses');
+    }
+  }
+  
+  
+
+  saveChanges(): void {
+    if (this.authService.isAdmin()) {
+      // Update the selected course with form values
+      const updatedCourse: Course = {
+        ...this.selectedCourse,
+        ...this.editCourseForm.value,
+      };
+
+      // Update the course in the database
+      this.courseService.updateCourse(this.selectedCourse.courseID, updatedCourse).subscribe(
+        (updatedCourse: Course) => {
+          console.log('Course updated successfully:', updatedCourse);
+          // Fetch all courses after successful update
+          this.fetchAllCourses();
+          // Close the edit modal
+          this.closeEditModal();
+        },
+        (error) => {
+          console.error('Error updating course:', error);
+        }
+      );
+    } else {
+      console.error('Only admins can update courses');
+    }
+  }
+
+
+  showDeleteConfirmation(course: Course): void {
+    // Set deleteConfirmationState for the specific course to true
+    this.deleteConfirmationState[course.courseID] = true;
+  }
+
+  // Method to confirm delete operation
+  deleteCourse(courseID: number): void {
+    if (this.authService.isAdmin()) {
+      // Perform the delete operation here
+      this.courseService.deleteCourse(courseID).subscribe(
+        () => {
+          console.log('Course deleted successfully');
+          // Fetch all courses after successful deletion
+          this.fetchAllCourses();
+        },
+        (error) => {
+          console.error('Error deleting course:', error);
+        }
+      );
+    } else {
+      console.error('Only admins can delete courses');
+    }
+
+    // Reset deleteConfirmationState for the specific course
+    this.deleteConfirmationState[courseID] = false;
+  }
+
+  // Method to cancel delete operation
+  cancelDelete(course: Course): void {
+    // Reset deleteConfirmationState for the specific course
+    this.deleteConfirmationState[course.courseID] = false;
+  }
+
+  closeEditModal(): void {
+    // Reset form and hide the edit modal
+    this.editCourseForm.reset();
+    this.editCourseModalVisible = false;
+  }
+  
   redirectToPayment(course: Course): void {
     // Navigate to the payment page using Router
     this.router.navigate(['/payment'], {
