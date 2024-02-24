d2e749ee7272a58de5f3810bff77de35a205aea0
neojarvis <techadmin@iamneo.ai>
2024-02-24 10:54:32 +0000

    Auto-save-commit-24/02/2024--16:24:30

--- British/angularapp/src/app/components/enquiry-list/enquiry-list.component.ts
+++ British/angularapp/src/app/components/enquiry-list/enquiry-list.component.ts
@@ -42,10 +42,22 @@ export class EnquiryListComponent implements OnInit {
       courseName: ['', Validators.required],
       emailID: ['', Validators.required],
       enquiryType: ['', Validators.required],
-      enquiryDate: ['', Validators.required],
+      // enquiryDate: ['', Validators.required],
+      enquiryDate: [this.getCurrentDateTime(), Validators.required],
     });
   }
 
+  getCurrentDateTime(): string {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+  }
+  
   fetchAllEnquiries(): void {
     this.enquiryService.getAllEnquiries().subscribe(
       (enquiries: Enquiry[]) => {
