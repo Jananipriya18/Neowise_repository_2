3abd98cf9399c9253407baaaccb94c026c2cd01b
neojarvis <techadmin@iamneo.ai>
2024-02-25 09:11:34 +0000

    Auto-save-commit-25/02/2024--14:41:32

--- British/angularapp/src/app/components/payment/payment.component.ts
+++ British/angularapp/src/app/components/payment/payment.component.ts
@@ -1,122 +1,4 @@
-// // Import necessary modules
-// import { Component, OnInit } from '@angular/core';
-// import { PaymentService } from '../../services/payment.service';
-// import { Payment } from '../../models/payment.model';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { AuthService } from '../../services/auth.service';
-// import { CourseService } from '../../services/course.service';
-// import { Course } from '../../models/course.model';
-
-// @Component({
-//   selector: 'app-payment',
-//   templateUrl: './payment.component.html',
-//   styleUrls: ['./payment.component.css']
-// })
-// export class PaymentComponent implements OnInit {
-//   courseName: string=''
-//   courseID: string=''
-//   amount: number = 0
-//   payments: Payment[] = [];
-//   newPayment: Payment = {
-//     paymentID: 0,
-//     userId: 0,
-//     courseID: 0,
-//     amountPaid: 0,
-//     paymentDate: new Date(),
-//     modeOfPayment: '',
-//   };
-
-//   courses: Course[] = [];
-//   createdPayment: Payment | null = null;
-
-//   constructor(
-//     private paymentService: PaymentService,
-//     private courseService: CourseService,
-//     private route: ActivatedRoute,
-//     private router: Router,
-//     private authService: AuthService
-//   ) {}
-
-//   ngOnInit(): void {
-//     this.route.queryParams.subscribe(params => {
-//       this.courseID = params['courseId'];
-//       this.courseName = params['courseName'];
-//       this.amount = params['amount'];
-//       // console.log(courseId);
-//       // console.log(courseName);
-      
-//     })
-//     this.newPayment.userId = Number(this.authService.getCurrentUserId());
-//     this.fetchAllCourses();
-//     // this.makePayment();
-//     console.log("New payment", this.newPayment);
-//   }
-
-//   // fetchAllPayments(): void {
-//   //   this.paymentService.getAllPayments().subscribe(
-//   //     (payments: Payment[]) => {
-//   //       this.payments = payments;
-//   //     },
-//   //     (error) => {
-//   //       console.error('Error fetching payments:', error);
-//   //     }
-//   //   );
-//   // }
-
-//   fetchAllCourses(): void {
-//     this.courseService.getAllCourses().subscribe(
-//       (courses: Course[]) => {
-//         console.log("Courses", courses, JSON.stringify(courses));
-//         this.courses = courses;
-//       },
-//       (error) => {
-//         console.error('Error fetching courses:', error);
-//       }
-//     );
-//   }
-
-//   makePayment(): void {
-//     console.log(this.newPayment);
-//     this.newPayment.courseID = parseInt(this.courseID);
-//     this.newPayment.amountPaid = this.amount;
-//     this.paymentService.createPayment(this.newPayment).subscribe(
-//       (createdPayment: Payment) => {
-//         console.log('Payment successful:', createdPayment);
-//         window.alert('Payment successful!');
-//         this.router.navigate(['/']);
-//         // this.fetchAllPayments();
-//       },
-//       (error) => {
-//         console.error('Error making payment:', error);
-//         window.alert('Error making Payment');
-//       }
-//     );
-//   }
-
-//   redirectToPayment(course: Course): void {
-//     if (this.authService.isStudent()) {
-//       const userId = this.authService.getCurrentUserId();
-      
-//       // console.log('Selected Course Information:', {
-//       //   courseId: course.courseID,
-//       //   courseName: course.courseName,
-//       // });
-//       console.log("Data check", JSON.stringify(course));
-  
-//       console.log('User Information:', {
-//         userId: userId,
-//       });
-  
-//       this.router.navigate(['/payment', course.courseID], {
-//         queryParams: {
-//           courseName: course.courseName,
-//           userId: userId,
-//         },
-//       });
-//     }
-//   }
-  
-// }
+// Import necessary modules
 import { Component, OnInit } from '@angular/core';
 import { PaymentService } from '../../services/payment.service';
 import { Payment } from '../../models/payment.model';
@@ -131,10 +13,10 @@ import { Course } from '../../models/course.model';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
-  courseName: string = '';
-  courseID: string = '';
-  amount: number = 0;
-  payments: any[] = [];
+  courseName: string=''
+  courseID: string=''
+  amount: number = 0
+  payments: Payment[] = [];
   newPayment: Payment = {
     paymentID: 0,
     userId: 0,
@@ -160,14 +42,20 @@ export class PaymentComponent implements OnInit {
       this.courseID = params['courseId'];
       this.courseName = params['courseName'];
       this.amount = params['amount'];
+      // console.log(courseId);
+      // console.log(courseName);
+      
     })
     this.newPayment.userId = Number(this.authService.getCurrentUserId());
     this.fetchAllCourses();
+    // this.makePayment();
+    console.log("New payment", this.newPayment);
   }
 
   fetchAllCourses(): void {
     this.courseService.getAllCourses().subscribe(
       (courses: Course[]) => {
+        console.log("Courses", courses, JSON.stringify(courses));
         this.courses = courses;
       },
       (error) => {
@@ -177,6 +65,7 @@ export class PaymentComponent implements OnInit {
   }
 
   makePayment(): void {
+    console.log(this.newPayment);
     this.newPayment.courseID = parseInt(this.courseID);
     this.newPayment.amountPaid = this.amount;
     this.paymentService.createPayment(this.newPayment).subscribe(
@@ -184,6 +73,7 @@ export class PaymentComponent implements OnInit {
         console.log('Payment successful:', createdPayment);
         window.alert('Payment successful!');
         this.router.navigate(['/']);
+        // this.fetchAllPayments();
       },
       (error) => {
         console.error('Error making payment:', error);
@@ -195,6 +85,17 @@ export class PaymentComponent implements OnInit {
   redirectToPayment(course: Course): void {
     if (this.authService.isStudent()) {
       const userId = this.authService.getCurrentUserId();
+      
+      // console.log('Selected Course Information:', {
+      //   courseId: course.courseID,
+      //   courseName: course.courseName,
+      // });
+      console.log("Data check", JSON.stringify(course));
+  
+      console.log('User Information:', {
+        userId: userId,
+      });
+  
       this.router.navigate(['/payment', course.courseID], {
         queryParams: {
           courseName: course.courseName,
@@ -203,4 +104,5 @@ export class PaymentComponent implements OnInit {
       });
     }
   }
+  
 }
