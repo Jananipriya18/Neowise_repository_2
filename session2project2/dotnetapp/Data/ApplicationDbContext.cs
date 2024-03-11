160ed0d0f725b4955302d5d105f12f1285952b35
neojarvis <techadmin@iamneo.ai>
2024-03-11 07:16:36 +0000

    Auto-save-commit-11/03/2024--12:46:37

---
+++ session2project2/dotnetapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using dotnetapp.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetapp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<GroceryItem> GroceryItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GroceryItem>()
+                .HasKey(g => g.ItemId); //ItemId is the primary key
+
+            modelBuilder.Entity<GroceryItem>().Property(g => g.ItemName).IsRequired();
+            modelBuilder.Entity<GroceryItem>().Property(g => g.Price).HasColumnType("decimal(18,2)");
+                
+        }
+
+    }
+}
+
