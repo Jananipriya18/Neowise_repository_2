2a1045dff0e48a15e5fb4380305ea492fb79a004
neojarvis <techadmin@iamneo.ai>
2024-03-12 01:24:32 +0000

    Auto-save-commit-12/03/2024--06:54:28

--- Appointment/dotnetapp/Controllers/AppointmentController.cs
+++ Appointment/dotnetapp/Controllers/AppointmentController.cs
@@ -1,5 +1,5 @@
+// AppointmentController.cs
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using dotnetapp.Models;
@@ -8,13 +8,94 @@ namespace dotnetapp.Controllers
 {
     public class AppointmentController : Controller
     {
-        private readonly BookDbContext _context;
+        private readonly ApplicationDbContext _context;
 
-        public AppointmentController(BookDbContext context)
+        public AppointmentController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        
+        public IActionResult Index()
+        {
+            var appointmentList = _context.Appointments.ToList();
+            return View(appointmentList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the appointment to the database
+                _context.Appointments.Add(appointment);
+                _context.SaveChanges();
+
+                // Redirect to the appointment list or another action
+                return RedirectToAction("Index");
+            }
+
+            // If the model state is not valid, return to the create view with validation errors
+            return View(appointment);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var appointment = _context.Appointments.Find(id);
+
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Appointments.Update(appointment);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(appointment);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var appointment = _context.Appointments.Find(id);
+
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        [HttpPost, ActionName("DeleteConfirmed")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var appointment = _context.Appointments.Find(id);
+
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Appointments.Remove(appointment);
+            _context.SaveChanges();
+            Console.lo
+            return RedirectToAction("Index");
+        }
     }
 }
