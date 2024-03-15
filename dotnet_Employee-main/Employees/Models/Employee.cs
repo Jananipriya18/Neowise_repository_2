f9b448d0a4f23ad5618c26fb16e32fe10afbf29d
neojarvis <techadmin@iamneo.ai>
2024-03-15 14:02:23 +0000

    Auto-save-commit-15/03/2024--19:32:17

---
+++ dotnet_Employee-main/Employees/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Employees.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Dob { get; set; }
+        public string Dept { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
