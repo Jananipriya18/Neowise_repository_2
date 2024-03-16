534a4f1d0ffd51798e543db6eac39699eeb8f689
neojarvis <techadmin@iamneo.ai>
2024-03-16 04:33:15 +0000

    Auto-save-commit-16/03/2024--10:03:11

---
+++ Feedback/TestProject/UnitTest1.cs
@@ -0,0 +1,111 @@
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using dotnetapp.Models;
+using dotnetapp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetapp.Tests
+{
+    [TestFixture]
+    public class AppointmentTests
+    {
+        private Type _appointmentControllerType;
+
+        [SetUp]
+        public void Setup()
+        {
+            Assembly assembly = Assembly.Load("dotnetapp");
+            _appointmentControllerType = assembly.GetType("dotnetapp.Controllers.AppointmentController");
+        }
+
+        [Test]
+        public void Test_Index_Action()
+        {
+            var indexMethod = _appointmentControllerType.GetMethod("Index");
+            var controllerInstance = Activator.CreateInstance(_appointmentControllerType);
+
+            var result = indexMethod.Invoke(controllerInstance, null) as IActionResult;
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public void Test_Create_Get_Action()
+        {
+            var createGetMethod = _appointmentControllerType.GetMethod("Create", new Type[] { });
+            var controllerInstance = Activator.CreateInstance(_appointmentControllerType);
+
+            var result = createGetMethod.Invoke(controllerInstance, null) as IActionResult;
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public void Test_Create_Post_Action()
+        {
+            var createPostMethod = _appointmentControllerType.GetMethod("Create", new Type[] { typeof(Appointment) });
+            var controllerInstance = Activator.CreateInstance(_appointmentControllerType);
+            var appointment = new Appointment();
+
+            var result = createPostMethod.Invoke(controllerInstance, new object[] { appointment }) as IActionResult;
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+        }
+
+        [Test]
+        public void Test_Edit_Get_Action()
+        {
+            var editGetMethod = _appointmentControllerType.GetMethod("Edit", new Type[] { typeof(int) });
+            var controllerInstance = Activator.CreateInstance(_appointmentControllerType);
+
+            var result = editGetMethod.Invoke(controllerInstance, new object[] { 1 }) as IActionResult;
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public void Test_Edit_Post_Action()
+        {
+            var editPostMethod = _appointmentControllerType.GetMethod("Edit", new Type[] { typeof(Appointment) });
+            var controllerInstance = Activator.CreateInstance(_appointmentControllerType);
+            var appointment = new Appointment { AppointmentID = 1 }; // Assuming an appointment with ID 1 exists
+
+            var result = editPostMethod.Invoke(controllerInstance, new object[] { appointment }) as IActionResult;
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            Assert.AreEqual("Index", (result as RedirectToActionResult).ActionName); // Ensure it redirects to Index action
+        }
+
+
+        [Test]
+        public void Test_Delete_Get_Action()
+        {
+            var deleteGetMethod = _appointmentControllerType.GetMethod("Delete", new Type[] { typeof(int) });
+            var controllerInstance = Activator.CreateInstance(_appointmentControllerType);
+
+            var result = deleteGetMethod.Invoke(controllerInstance, new object[] { 1 }) as IActionResult;
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public void  ()
+        {
+            var deleteConfirmedPostMethod = _appointmentControllerType.GetMethod("DeleteConfirmed", new Type[] { typeof(int) });
+            var controllerInstance = Activator.CreateInstance(_appointmentControllerType);
+
+            var result = deleteConfirmedPostMethod.Invoke(controllerInstance, new object[] { 1 }) as IActionResult;
+
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<RedirectToActionResult>(result);
+        }
+    }
+}
---
+++ Feedback/dotnetapp/Models/Feedback.cs
@@ -0,0 +1,29 @@
+﻿// FeedbackModel.cs
+
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace dotnetapp.Models{
+public class Feedback
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please enter your name.")]
+    [StringLength(100, ErrorMessage = "Name should not exceed 100 characters.")]
+    public string StudentName { get; set; }
+
+    [Required(ErrorMessage = "Please select the course.")]
+    public string Course { get; set; }
+
+    [Required(ErrorMessage = "Please provide feedback.")]
+    [StringLength(500, ErrorMessage = "Feedback should not exceed 500 characters.")]
+    public string Feedbacks { get; set; }
+
+    [Range(1, 5, ErrorMessage = "Rating should be between 1 and 5.")]
+    public int Rating { get; set; }
+
+    [Display(Name = "Date Submitted")]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    public DateTime DateSubmitted { get; set; } = DateTime.Now;
+}
+}
