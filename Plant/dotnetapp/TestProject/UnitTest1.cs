75e5661261c4b85c7cca03d995fdcbfed15166d9
neojarvis <techadmin@iamneo.ai>
2024-03-18 07:42:54 +0000

    Manual-save-commit-18/03/2024--13:12:50

--- Plant/dotnetapp/TestProject/UnitTest1.cs
+++ Plant/dotnetapp/TestProject/UnitTest1.cs
@@ -18,7 +18,7 @@ public class ApplicationTests
         _httpClient = new HttpClient();
         _httpClient.BaseAddress = new Uri("http://localhost:8080");
     }
-
+    
     [Test, Order(1)]
     public async Task Backend_TestRegisterAdmin()
     {
@@ -157,6 +157,87 @@ public class ApplicationTests
             new StringContent(plantJson, Encoding.UTF8, "application/json"));
 
         Assert.AreEqual(HttpStatusCode.Created, plantresponse.StatusCode);
+    }
+
+
+    [Test, Order(6)]
+    public async Task Backend_Test_Get_Plants_As_Admin()
+    {
+        string uniqueId = Guid.NewGuid().ToString();
+
+        // Generate a unique userName based on a timestamp
+        string uniqueUsername = $"abcd_{uniqueId}";
+        string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+        string requestBody = $"{{\"Username\": \"{uniqueUsername}\", \"Password\": \"abc@123A\", \"Email\": \"{uniqueEmail}\", \"MobileNumber\": \"1234567890\", \"UserRole\": \"Admin\"}}";
+        HttpResponseMessage response = await _httpClient.PostAsync("/api/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+        // Print registration response
+        string registerResponseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("Registration Response: " + registerResponseBody);
+
+        // Login with the registered user
+        string loginRequestBody = $"{{\"Email\" : \"{uniqueEmail}\",\"Password\" : \"abc@123A\"}}"; // Updated variable names
+        HttpResponseMessage loginResponse = await _httpClient.PostAsync("/api/login", new StringContent(loginRequestBody, Encoding.UTF8, "application/json"));
+
+        // Print login response
+        string loginResponseBody = await loginResponse.Content.ReadAsStringAsync();
+        Console.WriteLine("Login Response: " + loginResponseBody);
+
+        Assert.AreEqual(HttpStatusCode.OK, loginResponse.StatusCode);
+        string responseBody = await loginResponse.Content.ReadAsStringAsync();
+
+        dynamic responseMap = JsonConvert.DeserializeObject(responseBody);
+
+        string token = responseMap.token;
+
+        Assert.IsNotNull(token);
+
+        // Send GET request to retrieve plants
+        _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+        HttpResponseMessage plantsResponse = await _httpClient.GetAsync("/api/plants");
+
+        Assert.AreEqual(HttpStatusCode.OK, plantsResponse.StatusCode);
+    }
+
+    [Test, Order(7)]
+    public async Task Backend_Test_Get_Plants_As_Customer()
+    {
+        string uniqueId = Guid.NewGuid().ToString();
+
+        // Generate a unique userName based on a timestamp
+        string uniqueUsername = $"abcd_{uniqueId}";
+        string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+        string requestBody = $"{{\"Username\": \"{uniqueUsername}\", \"Password\": \"abc@123A\", \"Email\": \"{uniqueEmail}\", \"MobileNumber\": \"1234567890\", \"UserRole\": \"Customer\"}}";
+        HttpResponseMessage response = await _httpClient.PostAsync("/api/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+        // Print registration response
+        string registerResponseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("Registration Response: " + registerResponseBody);
+
+        // Login with the registered user
+        string loginRequestBody = $"{{\"Email\" : \"{uniqueEmail}\",\"Password\" : \"abc@123A\"}}"; // Updated variable names
+        HttpResponseMessage loginResponse = await _httpClient.PostAsync("/api/login", new StringContent(loginRequestBody, Encoding.UTF8, "application/json"));
+
+        // Print login response
+        string loginResponseBody = await loginResponse.Content.ReadAsStringAsync();
+        Console.WriteLine("Login Response: " + loginResponseBody);
+
+        Assert.AreEqual(HttpStatusCode.OK, loginResponse.StatusCode);
+        string responseBody = await loginResponse.Content.ReadAsStringAsync();
+
+        dynamic responseMap = JsonConvert.DeserializeObject(responseBody);
+
+        string token = responseMap.token;
+
+        Assert.IsNotNull(token);
+
+        // Send GET request to retrieve plants
+        _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+        HttpResponseMessage plantsResponse = await _httpClient.GetAsync("/api/plants");
+
+        Assert.AreEqual(HttpStatusCode.OK, plantsResponse.StatusCode);
     }
 
 }
