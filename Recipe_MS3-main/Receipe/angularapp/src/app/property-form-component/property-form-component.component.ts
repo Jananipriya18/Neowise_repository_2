a18e0a25576f9d02624931d31868cbe9e204dc0a
neojarvis <techadmin@iamneo.ai>
2024-04-16 10:13:23 +0000

    Auto-save-commit-16/04/2024--15:43:22

--- Recipe_MS3-main/Receipe/angularapp/src/app/property-form-component/property-form-component.component.ts
+++ Recipe_MS3-main/Receipe/angularapp/src/app/property-form-component/property-form-component.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Property } from '../models/property.model';
+import { PropertyService } from '../services/property.service'; // Corrected import statement
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-property-form-component',
-  templateUrl: './property-form-component.component.html',
-  styleUrls: ['./property-form-component.component.css']
+  selector: 'app-property-form',
+  templateUrl: './property-form.component.html',
+  styleUrls: ['./property-form.component.css']
 })
-export class PropertyFormComponentComponent implements OnInit {
+export class PropertyFormComponent {
+  newProperty: Property = {
+    propertyId: 0,
+    // Include other properties from the Property model as needed
+    // For example: name: '', address: '', etc.
+  };
 
-  constructor() { }
+  constructor(private propertyService: PropertyService, private router: Router) { }
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    this.propertyService.addProperty(this.newProperty).subscribe(() => {
+      console.log('Property added successfully!');
+      this.router.navigate(['/viewProperties']);
+    });
   }
-
 }
