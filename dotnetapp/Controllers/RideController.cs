35f28c191631c76942beb74554f416f040444e1d
neojarvis <techadmin@iamneo.ai>
2024-04-26 09:26:20 +0000

    Auto-save-commit-26/04/2024--14:56:19

--- dotnetapp/Controllers/RideController.cs
+++ dotnetapp/Controllers/RideController.cs
@@ -1,68 +1,68 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// // using gym.Data;
-// using RideShare.Exceptions; 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+// using gym.Data;
+using RideShare.Exceptions; 
 
-// using RideShare.Models;
-// using System;
-// using System.Linq;
-// using System.Threading.Tasks;
+using RideShare.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
-// namespace RideShare.Controllers
-// {
-// public class RideController : Controller
-// {
-//     private readonly RideSharingDbContext _dbContext;
+namespace RideShare.Controllers
+{
+public class RideController : Controller
+{
+    private readonly RideSharingDbContext _dbContext;
 
-//     public RideController(RideSharingDbContext dbContext)
-//     {
-//         _dbContext = dbContext;
-//     }
+    public RideController(RideSharingDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 
-//     public IActionResult AvailableRides()
-// {
-//     var rides = _dbContext.Rides.Include(r => r.Commuters).ToList();
-//     return View(rides);
-// }
+    public IActionResult AvailableRides()
+{
+    var rides = _dbContext.Rides.Include(r => r.Commuters).ToList();
+    return View(rides);
+}
 
 
-//     public IActionResult Details(int id)
-//     {
-//         var ride = _dbContext.Rides.Include(r => r.Commuters).FirstOrDefault(r => r.RideID == id);
-//         if (ride == null)
-//         {
-//             return NotFound();
-//         }
+    public IActionResult Details(int id)
+    {
+        var ride = _dbContext.Rides.Include(r => r.Commuters).FirstOrDefault(r => r.RideID == id);
+        if (ride == null)
+        {
+            return NotFound();
+        }
 
-//         int joinedCommuters = ride.Commuters.Count;
-//         int availableSeats = ride.MaximumCapacity - joinedCommuters;
+        int joinedCommuters = ride.Commuters.Count;
+        int availableSeats = ride.MaximumCapacity - joinedCommuters;
 
-//         ViewBag.JoinedCommuters = joinedCommuters;
-//         ViewBag.AvailableSeats = availableSeats;
+        ViewBag.JoinedCommuters = joinedCommuters;
+        ViewBag.AvailableSeats = availableSeats;
 
-//         return View(ride);
-//     }
-//         public IActionResult DeleteConfirm(int id)
-//         {
-//             var ride = _dbContext.Rides.Include(r => r.Commuters).FirstOrDefault(r => r.RideID == id);
+        return View(ride);
+    }
+        public IActionResult DeleteConfirm(int id)
+        {
+            var ride = _dbContext.Rides.Include(r => r.Commuters).FirstOrDefault(r => r.RideID == id);
 
-//             return View(ride);
-//         }
-//             [HttpPost]
-//         public IActionResult Delete(int id)
-//         {
-//             Console.WriteLine("de;e"+id);
-//             var ride = _dbContext.Rides.Find(id);
-//             if (ride == null)
-//             {
-//                 return NotFound();
-//             }
+            return View(ride);
+        }
+            [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Console.WriteLine("de;e"+id);
+            var ride = _dbContext.Rides.Find(id);
+            if (ride == null)
+            {
+                return NotFound();
+            }
 
-//             _dbContext.Rides.Remove(ride);
-//              _dbContext.SaveChangesAsync();
+            _dbContext.Rides.Remove(ride);
+             _dbContext.SaveChangesAsync();
 
-//             return RedirectToAction(nameof(AvailableRides));
-//         }
+            return RedirectToAction(nameof(AvailableRides));
+        }
 
-//     }
-// }
+    }
+}
