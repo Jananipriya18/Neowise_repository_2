cb0ce25fbabf99e871d63dd806beed4abae734ea
neojarvis <techadmin@iamneo.ai>
2024-05-11 14:07:18 +0000

    Auto-save-commit-11/05/2024--19:37:14

--- Remote_Tutoring_App/angularapp/src/app/app.module.ts
+++ Remote_Tutoring_App/angularapp/src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import { TutorFormComponent } from './tutor-form/tutor-form.component';
 import { HeaderComponent } from './header/header.component';
 import { TutorListComponent } from './tutor-list/tutor-list.component';
 import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.component';
-
-
-
 
 @NgModule({
   declarations: [
@@ -25,7 +21,7 @@ import { DeleteConfirmComponent } from './delete-confirm/delete-confirm.componen
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
