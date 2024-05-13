c2f9d5e9a37d6001cc4b7343442a7019069f8dcc
neojarvis <techadmin@iamneo.ai>
2024-05-13 06:08:05 +0000

    Auto-save-commit-13/05/2024--11:38:05

--- Remote_Tutoring_App/angularapp/src/app/tutor-form/tutor-form.component.spec.ts
+++ Remote_Tutoring_App/angularapp/src/app/tutor-form/tutor-form.component.spec.ts
@@ -1,54 +1,54 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { RouterTestingModule } from '@angular/router/testing';
-import { RecipeFormComponent } from './recipe-form.component';
-import { RecipeService } from '../services/recipe.service';
+import { TutorFormComponent } from './tutor-form.component';
+import { TutorService } from '../services/tutor.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Router } from '@angular/router';
 import { of } from 'rxjs';
-import { Recipe } from '../models/recipe.model';
+import { Tutor } from '../models/tutor.model';
 import { fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-import { RecipeListComponent } from '../recipe-list/recipe-list.component';
+import { TutorListComponent } from '../tutor-list/tutor-list.component';
  
-describe('RecipeFormComponent', () => {
-  let component: RecipeFormComponent;
-  let fixture: ComponentFixture<RecipeFormComponent>;
-  let recipeService: RecipeService;
+describe('TutorFormComponent', () => {
+  let component: TutorFormComponent;
+  let fixture: ComponentFixture<TutorFormComponent>;
+  let tutorService: TutorService;
   let router: Router;
-  let recipeListComponent: RecipeListComponent;
+  let tutorListComponent: TutorListComponent;
  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RecipeFormComponent],
+      declarations: [TutorFormComponent],
       imports: [FormsModule, RouterTestingModule, HttpClientTestingModule],
       providers: [
-        RecipeService,
+        TutorService,
       ]
     })
       .compileComponents();
   });
  
   beforeEach(() => {
-    fixture = TestBed.createComponent(RecipeFormComponent);
+    fixture = TestBed.createComponent(TutorFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
  
-    recipeService = TestBed.inject(RecipeService);
+    tutorService = TestBed.inject(TutorService);
     router = TestBed.inject(Router);
  
   });
  
-  fit('should_create_RecipeFormComponent', () => {
+  fit('should_create_TutorFormComponent', () => {
     expect(component).toBeTruthy();
 });
  
  
-  fit('RecipeFormComponent_should_render_error_messages_when_required_fields_are_empty_on_submit', () => {
+  fit('TutorFormComponent_should_render_error_messages_when_required_fields_are_empty_on_submit', () => {
   // Set all fields to empty strings
-  component.newRecipe = {
-    recipeId: null,
+  component.newTutor = {
+    tutorId: null,
     name: '',
     email: '',
     subjectsOffered: '',
@@ -77,17 +77,17 @@ describe('RecipeFormComponent', () => {
   expect(fixture.debugElement.query(By.css('#availability + .error-message'))).toBeTruthy();
 });
  
-  fit('RecipeFormComponent_should_call_add_recipe_method_while_adding_the_recipe', () => {
-    // Create a mock Recipe object with all required properties
-    const recipe: Recipe = { 
-      recipeId: 1, 
-      name: 'Test Recipe', 
-      email: 'Test Recipe Email', 
+  fit('TutorFormComponent_should_call_add_tutor_method_while_adding_the_tutor', () => {
+    // Create a mock Tutor object with all required properties
+    const tutor: Tutor = { 
+      tutorId: 1, 
+      name: 'Test Tutor', 
+      email: 'Test Tutor Email', 
       subjectsOffered: ' Test subjects Offered', 
       contactNumber: 'Test Contact Number', 
       availability: 'Test Availability'
     } as any;
-    const addRecipeSpy = spyOn(component, 'addTutor').and.callThrough();
+    const addTutorSpy = spyOn(component, 'addTutor').and.callThrough();
     component.addTutor();
     expect(addTutorSpy).toHaveBeenCalled();
   });
