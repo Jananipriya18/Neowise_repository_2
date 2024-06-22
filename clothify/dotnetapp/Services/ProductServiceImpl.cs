1072296f65c57df4a6ab444af7f24e939a4bb02f
neojarvis <techadmin@iamneo.ai>
2024-06-22 02:36:52 +0000

    Manual-save-commit-22/06/2024--08:06:51

---
+++ clothify/dotnetapp/Services/ProductServiceImpl.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using dotnetapp.Models;
+using dotnetapp.Services;
+public class GiftServiceImpl : GiftService
+{
+    private readonly GiftRepository _giftRepository;
+
+    public GiftServiceImpl(GiftRepository giftRepository)
+    {
+        _giftRepository = giftRepository;
+    }
+
+    public Gift AddGift(Gift gift)
+    {
+        return _giftRepository.addGift(gift);
+    }
+
+    public List<Gift> GetAllGifts()
+    {
+        return _giftRepository.getAllGifts();
+    }
+
+    public Gift EditGift(long giftId, Gift updatedGift)
+    {
+        return _giftRepository.editGift(giftId, updatedGift);
+    }
+
+    public Gift DeleteGift(long giftId)
+    {
+        return _giftRepository.deleteGift(giftId);
+    }
+}
